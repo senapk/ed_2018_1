4eba41cddbef0d15b925ee5c98d2b317248e8672
David Sena <sena.ufc@gmail.com>
2018-04-24 17:27:01 -0300

    up

--- s07e02_lista/lista/main.cpp
+++ s07e02_lista/lista/main.cpp
@@ -20,6 +20,22 @@ struct SList{
         this->head = nullptr;
     }
 
+    ~SList(){
+        deletarTudo(head);
+    }
+
+    void deletarTudo(Node * node){
+
+    }
+
+    5 4 7 8 9 3 1
+    cortarRabo(8)
+    5 4 7
+    //procura o valor, remove desse no em diante
+    Node * cortarRabo(Node * node, int value){
+
+    }
+
     void push_front(int value){
         //this->head = new Node(value, this->head);
         Node * node = new Node(value);
@@ -35,12 +51,12 @@ struct SList{
         delete aux;
     }
 
-    void push_back(int value){ //todo
+    void push_back(int value){
         if(head == nullptr){
             head = new Node(value);
             return;
         }
-        auto node = new Node();
+        auto node = head;
         while(node->next != nullptr)
             node = node->next;
         node->next = new Node(value);
@@ -89,8 +105,8 @@ private:
     void _rshow(Node * node){
         if(node == nullptr)
             return;
-        _rshow(node->next);
         cout << node->value << " ";
+        _rshow(node->next);
     }
 public:
     void rshow(){
@@ -141,11 +157,17 @@ node700:{4 250};
     }
 
     Node * _rinserir_ordenado(Node * node, int value){
+        return nullptr;
         //todo
     }
 
     void rinserir_ordenado(int value){
         //todo
+    }
+
+    //todo
+    int size(){
+        return 0;
     }
 
     int rsomar(Node * node){
@@ -160,10 +182,66 @@ node700:{4 250};
 
 };
 
-
-int main()
-{
+int main(){
     SList lista;
-    lista._rshow(lista.head);
+    lista.push_back(5);
+    lista.push_back(6);
+    lista.push_back(7);
+    lista.push_back(10);
     return 0;
 }
+
+
+//int main()
+//{
+//    SList lista;
+//    lista._rshow(lista.head);
+//    return 0;
+//}
+
+////int mainc(){
+////    char produto[] = "nescau 200 ml 10.00 5 laticineos";
+////    char str[50];
+////    float valor = 4.50;
+////    int qtd = 10;
+////    char setor[] = "hortifruti";
+////    sprintf(str, "uva %f %d %s", valor, qtd, setor);
+////    char nome[50];
+////    sscanf(produto, "%s %f %d %s", nome, &valor, &qtd, setor);
+////    return 0;
+////}
+
+////#include <fstream>
+
+////int maincpp(){
+////    int x;
+////    ofstream asaida("../lista/saida.txt");
+////    ifstream ainput("../lista/entrada.txt");
+////    string linha;
+////    getline(ainput, linha, ';');
+////    asaida << "digite um inteiro" << endl;
+////    ainput >> x;
+////    asaida << "digite um nome bonito" << endl;
+////    string s;
+////    ainput >> s;
+////    asaida << "x = " << x << ", s = " << s << endl;
+
+////    return 0;
+////}
+
+////#include <sstream>
+////int main(){
+////    stringstream ss;
+////    ss << "hoje " << 24 << "abril de 2014, temp=" << 27.73 << endl;
+////    cout << ss.str();
+
+////    string str("44 38");
+////    stringstream ss2(str);
+////    int lixo;
+////    int i;
+////    ss2 >> lixo >> i;
+////    return 0;
+////}
+
+
+
